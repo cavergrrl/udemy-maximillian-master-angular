d7d0fdf2c40de5a630238b30ab1b3632c7ddf57c
Ellie <ellie_aggie01@yahoo.com>
2025-01-30 16:48:46 -0600

    rename title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'udemy-maximillian-master-angular' title`, () => {
+  it(`should have the 'Udemy - Master Angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('udemy-maximillian-master-angular');
+    expect(app.title).toEqual('Udemy - Master Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, udemy-maximillian-master-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Udemy - Master Angular');
   });
 });
