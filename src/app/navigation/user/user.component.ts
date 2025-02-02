f5f14b47b7fadb5cb8637a0b330d54e8951577c5
Ellie <ellie_aggie01@yahoo.com>
2025-02-02 15:43:30 -0600

    show avatar & name for 1 random user to display

--- src/app/navigation/user/user.component.ts
+++ src/app/navigation/user/user.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { USERS } from '../../../mock/users';
+
+const randomIndex = Math.floor(Math.random() * USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  user = USERS[randomIndex];
 }
