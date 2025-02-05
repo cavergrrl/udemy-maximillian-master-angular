f7a7faa8505bf51831791c5c99dece232f49f81c
Ellie <ellie_aggie01@yahoo.com>
2025-02-04 23:17:35 -0600

    use pipe to show values using  currency format

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,9 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {AnnualData} from '../model/annualData';
+import {CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
-  imports: [],
+  imports: [
+    CurrencyPipe
+  ],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
