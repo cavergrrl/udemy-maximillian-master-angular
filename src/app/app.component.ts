35950fb585101cd970d3274a015a06d979a392f1
Ellie <ellie_aggie01@yahoo.com>
2025-02-04 22:59:39 -0600

    refactor: rename to onCalculate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
 
   constructor(private investmentService: InvestmentService) {}
 
-  calculate(investmentData:InvestmentData) {
+  onCalculate(investmentData:InvestmentData) {
     this.investmentService.calculateInvestment(investmentData);
   }
 }
