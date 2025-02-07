f4b0d6baea69c1aeeb324f370b79af3400440ceb
Ellie <ellie_aggie01@yahoo.com>
2025-02-06 21:55:27 -0600

    button.component uses attribute selectors

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import {ConfigService} from "../service/config.service";
+import {ButtonComponent} from "../shared/button/button.component";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+    imports: [
+        ButtonComponent
+    ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
