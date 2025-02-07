94fca23ea7811ab8825f81aa3f7b55f7b2e5ba31
Ellie <ellie_aggie01@yahoo.com>
2025-02-06 23:36:51 -0600

    refactor: use ngOnDestroy to cleanup setInterval() - possible memory leak

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {DashboardItemComponent} from '../dashboard-item/dashboard-item.component';
 
 @Component({
@@ -9,15 +9,17 @@ import {DashboardItemComponent} from '../dashboard-item/dashboard-item.component
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit{
+export class ServerStatusComponent implements OnInit, OnDestroy {
   title = 'Server Status';
   image = { src: 'status.png', alt: 'Server' };
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
 
+  private interval?: ReturnType<typeof setInterval>;
+
   constructor() {}
 
   ngOnInit() {
-    setInterval(() => {
+    this.interval = setInterval(() => {
       const random = Math.random();
       if (random < 0.5) {
         this.currentStatus = 'online';
@@ -27,5 +29,9 @@ export class ServerStatusComponent implements OnInit{
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.interval);
   }
 }
