b7fd35ec35b9b97523ca89857a2472cf4695845e
Ellie <ellie_aggie01@yahoo.com>
2025-02-06 21:31:33 -0600

    support-tickets uses new-ticket

--- src/app/dashboard/support-tickets/support-tickets.component.ts
+++ src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {DashboardItemComponent} from '../dashboard-item/dashboard-item.component';
+import {NewTicketComponent} from '../tickets/new-ticket/new-ticket.component';
 
 @Component({
   selector: 'app-support-tickets',
   imports: [
-    DashboardItemComponent
+    DashboardItemComponent,
+    NewTicketComponent
   ],
   templateUrl: './support-tickets.component.html',
   styleUrl: './support-tickets.component.css'
