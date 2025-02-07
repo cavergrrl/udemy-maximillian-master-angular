aed9774834bbd503554486e4b7a91868586c1be4
Ellie <ellie_aggie01@yahoo.com>
2025-02-06 20:17:20 -0600

    support-tickets that uses dashboard-item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {ServerStatusComponent} from './dashboard/server-status/server-status.component';
 import {TrafficComponent} from './dashboard/traffic/traffic.component';
+import {SupportTicketsComponent} from './dashboard/support-tickets/support-tickets.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
     ServerStatusComponent,
-    TrafficComponent
+    TrafficComponent,
+    SupportTicketsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
---
+++ src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {DashboardItemComponent} from '../dashboard-item/dashboard-item.component';
+
+@Component({
+  selector: 'app-support-tickets',
+  imports: [
+    DashboardItemComponent
+  ],
+  templateUrl: './support-tickets.component.html',
+  styleUrl: './support-tickets.component.css'
+})
+export class SupportTicketsComponent {
+
+}
