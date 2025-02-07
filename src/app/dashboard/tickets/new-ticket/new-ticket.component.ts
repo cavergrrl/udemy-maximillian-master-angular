4d8e4dc03fa6f344b854ee7fb179c9801da5bf61
Ellie <ellie_aggie01@yahoo.com>
2025-02-07 00:00:23 -0600

    refactor: in new-ticket, store form inputs in a Template Var - for titleInput pass in HTMLInputElement - for requestInput pass in only the HTMLInputElement.value

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import {ControlComponent} from '../../../shared/control/control.component';
 import {ButtonComponent} from "../../../shared/button/button.component";
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   imports: [
     ControlComponent,
-    ButtonComponent
+    ButtonComponent,
+    FormsModule
   ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
 
+  onSubmit(titleInput: HTMLInputElement, requestInput: string) {
+    console.dir(titleInput);
+    console.log("get value for titleInput in HTMLInputElement --> " + titleInput.value);
+    console.log("requestInput that only received the HTMLInputElement.value --> " + requestInput);
+  }
 }
