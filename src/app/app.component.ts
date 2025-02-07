448f69e1f8f1f1cf802068f44daf17a93dccbb28
Ellie <ellie_aggie01@yahoo.com>
2025-02-06 20:07:46 -0600

    server-status that uses dashboard-item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { ConfigService } from './service/config.service';
 import {TrafficService} from './service/traffic.service';
 import {HeaderComponent} from './header/header.component';
+import {ServerStatusComponent} from './dashboard/server-status/server-status.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    ServerStatusComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  currentStatus = 'online';
   maxTraffic: number = 1;
 
   constructor(private configService: ConfigService,
