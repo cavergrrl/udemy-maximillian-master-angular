06e8329b43914a9feaa74b18413ce8cea2a56627
Ellie <ellie_aggie01@yahoo.com>
2025-02-06 20:14:48 -0600

    traffic that uses dashboard-item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,18 @@
 import { Component } from '@angular/core';
-import { ConfigService } from './service/config.service';
-import {TrafficService} from './service/traffic.service';
 import {HeaderComponent} from './header/header.component';
 import {ServerStatusComponent} from './dashboard/server-status/server-status.component';
+import {TrafficComponent} from './dashboard/traffic/traffic.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
-    ServerStatusComponent
+    ServerStatusComponent,
+    TrafficComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  maxTraffic: number = 1;
 
-  constructor(private configService: ConfigService,
-              private trafficService: TrafficService
-  ) {}
-
-  ngOnInit() {
-    this.maxTraffic = this.trafficService.calculateMaxTraffic();
-  }
-
-  get trafficData() {
-    return this.trafficService.getTrafficData();
-  }
 }
---
+++ src/app/dashboard/traffic/traffic.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {DashboardItemComponent} from '../dashboard-item/dashboard-item.component';
+import {TrafficService} from '../../service/traffic.service';
+
+@Component({
+  selector: 'app-traffic',
+  imports: [
+    DashboardItemComponent
+  ],
+  templateUrl: './traffic.component.html',
+  styleUrl: './traffic.component.css'
+})
+export class TrafficComponent {
+  maxTraffic: number = 1;
+
+  constructor(private trafficService: TrafficService) {}
+
+  ngOnInit() {
+    this.maxTraffic = this.trafficService.calculateMaxTraffic();
+  }
+
+  get trafficData() {
+    return this.trafficService.getTrafficData();
+  }
+}
