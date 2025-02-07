970733128124faf16da490f14b8396c81334860c
Ellie <ellie_aggie01@yahoo.com>
2025-02-06 22:28:05 -0600

    control.component expects select for form input/textarea

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {ControlComponent} from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [],
+  imports: [
+    ControlComponent
+  ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
