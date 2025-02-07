bb780bca16426b8d3fc33395efd56453f8368ea9
Ellie <ellie_aggie01@yahoo.com>
2025-02-06 22:45:45 -0600

    fix: new-ticket.component needs to import ButtonComponent

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {ControlComponent} from '../../../shared/control/control.component';
+import {ButtonComponent} from "../../../shared/button/button.component";
 
 @Component({
   selector: 'app-new-ticket',
   imports: [
-    ControlComponent
+    ControlComponent,
+    ButtonComponent
   ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
